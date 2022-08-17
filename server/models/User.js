f83034dae79e3e65c0d1f7ab02d0e0c408cc448a
void2412 <void2412@gmail.com>
2022-08-17 18:33:17 +0930

    added authentication for User model

--- server/models/User.js
+++ server/models/User.js
@@ -36,6 +36,18 @@ userSchema.virtual('contactCount').get(function(){
 	return this.contacts.length
 })
 
+userSchema.pre('save', async function(next){
+	if(this.isNew || this.isModified('password')){
+		const saltRounds = 10
+		this.password = await bcrypt.hash(this.password, saltRounds)
+	}
+	next()
+})
+
+userSchema.methods.isCorrectPassword = async function(password){
+	return bcrypt.compare(password, this.password)
+}
+
 const User = model('User', userSchema)
 
 module.exports = User
