90db659851bc30c585c22422661dbe484e719ccd
void2412 <void2412@gmail.com>
2022-08-20 21:59:24 +0930

    finished mutations and queries on front end:

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,28 +27,133 @@ mutation AddUser($name: String!, $email: String!, $password: String!){
 `
 
 export const addContact = gql`
+mutation AddContact($userId: ID!) {
+  addContact(userId: $userId) {
+    _id
+    email
+    name
+    contacts {
+      _id
+      name
+      email
+    }
+    conversations {
+      _id
+      members {
+        _id
+        name
+        email
+      }
+      messages {
+        _id
+        text
+        user {
+          _id
+          name
+        }
+      }
+    }
+  }
+}
 `
 
 export const removeContact = gql`
-
+mutation AddContact($userId: ID!) {
+  removeContact(userId: $userId) {
+    _id
+    email
+    name
+    contacts {
+      _id
+      email
+      name
+    }
+  }
+}
 `
 
 export const addConversation = gql`
-
+mutation AddConversation($members: [ID]!) {
+  addConversation(members: $members) {
+    _id
+    messages {
+      _id
+      user {
+        _id
+        name
+        email
+      }
+      text
+    }
+    members {
+      _id
+      name
+      email
+    }
+  }
+}
 `
 
 export const removeConversationFromUser = gql`
-
+mutation AddConversation($conversationId: ID!) {
+  removeConversationFromUser(conversationId: $conversationId) {
+    _id
+    email
+    name
+    conversations {
+      _id
+    }
+  }
+}
 `
 
 export const addMessage = gql`
-
+mutation AddMessage($conversationId: ID!, $text: String!) {
+  addMessage(conversationId: $conversationId, text: $text) {
+    _id
+    messages {
+      _id
+      text
+      user {
+        _id
+        email
+        name
+      }
+    }
+  }
+}
 `
 
 export const removeMessage = gql`
-
+mutation RemoveMessage($messageId: ID!) {
+  removeMessage(messageId: $messageId) {
+    _id
+    messages {
+      _id
+      user {
+        _id
+        email
+        name
+      }
+      text
+    }
+  }
+}
 `
 
 export const editMessage = gql`
-
+mutation RemoveMessage($messageId: ID!, $text: String!) {
+  editMessage(messageId: $messageId, text: $text) {
+    _id
+    messages {
+      _id
+      user {
+        _id
+        email
+        name
+      }
+      text
+    }
+  }
+}
 `
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -36,3 +36,28 @@ export const QUERY_ME = gql`
 	}
   }
 `
+
+export const QUERY_CONVERSATIONS = gql`
+query Conversations {
+  conversations {
+    _id
+    members {
+      _id
+      name
+    }
+  }
+}
+`
+
+export const QUERY_MESSAGES = gql`
+query Messages($conversationId: ID!) {
+  messages(conversationId: $conversationId) {
+    _id
+    text
+    user {
+      _id
+      name
+    }
+  }
+}
+`
